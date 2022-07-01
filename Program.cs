9fce1b943d221c8add817c95f5da97440b49e426
IvanSokolov <keenfalcon@mail.ru>
2022-07-01 20:54:45 +0300

    Добавил задачу

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿string[] array = new string[] { "hello", "world", "visual", "studio", "code", "kva", "it", "work", "q" };
+string[] group = Array.FindAll(array, arrayLessFour => arrayLessFour.Length <= 3);
+foreach (var arrayLessFour in group)
+    Console.WriteLine(arrayLessFour);
+Console.WriteLine(" ");
